35f9d63100190df218697d97775473b42a3a7fe3
hiroyuki inoue <syslink.h.inoue@gmail.com>
2012-11-29 01:08:29 +0900

    first commit.

---
+++ gen/com/ino_h/languagememo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ino_h.languagememo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/ino_h/languagememo/LanguageMemoActivity.java
@@ -0,0 +1,13 @@
+package com.ino_h.languagememo;
+
+import org.apache.cordova.DroidGap;
+import android.os.Bundle;
+
+public class LanguageMemoActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+}
