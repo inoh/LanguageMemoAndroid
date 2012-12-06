201c10284e42aef770e4c1b2f038f8d4ba4e9f9e
hiroyuki inoue <syslink.h.inoue@gmail.com>
2012-12-06 22:22:30 +0900

    指定したメモに対して写真をアップロードする

--- assets/www/js/language.memo.js
+++ assets/www/js/language.memo.js
@@ -1,24 +1,24 @@
 var lm = (function () {
-	//var APPLICATION_URL = "http://lmt.ruby.iijgio.com";
+	//var APPLICATION_URL = "http://lmt.ruby.iijgio.com"
 	var APPLICATION_URL = "http://192.168.1.25:3000";
-	
-	var pictureUpload = function (id, sourceType) {
+	var memo_id;
+	var pictureUpload = function (sourceType) {
 		navigator.camera.getPicture(function (imageUri) {
 				var options = new FileUploadOptions(),
 				    transfer = new FileTransfer(),
 				    success = function () {
-						alert ("アップロードが完了しました。");
+						alert("アップロードが完了しました。");
 						$('.ui-dialog').dialog('close');
 				    },
 				    error = function (error) {
-						$("#capture_label").html("ERROR: " + error.code);
+						alert("ERROR: " + error.code);
 			    		throw "ERROR: " + error.code;
 				    };
 
 				options.fileKey = "document[upload_data]";
 				options.fileName = imageUri.substr(imageUri.lastIndexOf('/')+1);
 				options.mimeType = "image/jpeg";
-				transfer.upload(imageUri, APPLICATION_URL + "/memos/" + id + "/documents.json", success, error, options);
+				transfer.upload(imageUri, APPLICATION_URL + "/memos/" + memo_id + "/documents.json", success, error, options);
 			},
 			function (message) {
 				$("#capture_label").html(message);
@@ -35,10 +35,13 @@ var lm = (function () {
 	return {
 		URL: APPLICATION_URL,
 		getCamera: function () {
-			pictureUpload (1, navigator.camera.PictureSourceType.CAMERA);
+			pictureUpload (navigator.camera.PictureSourceType.CAMERA);
 		},
 		getLibrary: function () {
-			pictureUpload (1, navigator.camera.PictureSourceType.PHOTOLIBRARY);
+			pictureUpload (navigator.camera.PictureSourceType.PHOTOLIBRARY);
+		},
+		setId: function (id) {
+			memo_id = id;
 		}
 	}
 })();
